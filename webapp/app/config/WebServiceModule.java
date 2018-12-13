7ab399e9041c8a336621e4be9f981f2f1537f2be
Gustavo Gomez <ggomez@digitalgeko.com>
2018-12-13 17:02:45 -0600

    Correcciones de ambiente e IP de servers

--- webapp/app/config/WebServiceModule.java
+++ webapp/app/config/WebServiceModule.java
@@ -10,11 +10,16 @@ public class WebServiceModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		bind(PolicyService.class).to(PolicyServiceImpl.class);
+		bind(JsonService.class).to(JsonServiceImpl.class);
 		bind(CreateRequestService.class).to(CreateRequestServiceImpl.class);
 		bind(InspectionService.class).to(InspectionServiceImpl.class);
 		bind(PolicyFileService.class).to(PolicyFileServiceFtpImpl.class);
 		bind(ClientsQueryWebService.class).to(ClientsQueryServiceBusImpl.class);
 		bind(AverageValueQueryWebService.class).to(AverageValueQueryServiceBusImpl.class);
+		bind(PersonQueryWebService.class).to(PersonQueryServiceBusImpl.class);
+		bind(BusinessQueryWebService.class).to(BusinessQueryServiceBusImpl.class);
+		bind(PolicyInputWebService.class).to(PolicyInputServiceBusImpl.class);
+		bind(InsurableVehicleQueryWebService.class).to(InsurableVehicleQueryServiceBusImpl.class);
 	}
 
 }
