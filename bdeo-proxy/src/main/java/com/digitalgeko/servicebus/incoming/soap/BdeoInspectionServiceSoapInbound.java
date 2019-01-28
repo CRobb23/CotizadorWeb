4e8ac0b77987408fe026dbfd59191ee6db61c356
Gustavo Gomez <ggomez@digitalgeko.com>
2019-01-28 08:41:01 -0600

    Correcciones a envio BDEO

--- bdeo-proxy/src/main/java/com/digitalgeko/servicebus/incoming/soap/BdeoInspectionServiceSoapInbound.java
+++ bdeo-proxy/src/main/java/com/digitalgeko/servicebus/incoming/soap/BdeoInspectionServiceSoapInbound.java
@@ -3,6 +3,8 @@ package com.digitalgeko.servicebus.incoming.soap;
 import com.digitalgeko.servicebus.service.BdeoInspectionsBus;
 import corpbi.BdeoInspectionRequest;
 import corpbi.BdeoInspectionResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
@@ -14,12 +16,15 @@ public class  BdeoInspectionServiceSoapInbound {
 
     private static final String NAMESPACE_URI = "CorpBI";
 
+    protected static final Logger log = LoggerFactory.getLogger(BdeoInspectionServiceSoapInbound.class);
+
     @Autowired
     private BdeoInspectionsBus bdeoInspectionsBus;
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "bdeoInspectionRequest")
     @ResponsePayload
     public BdeoInspectionResponse updateInspectionState(@RequestPayload BdeoInspectionRequest request) {
+        log.info(request.getMessage());
         String responseMessage = bdeoInspectionsBus.parseBdeoMessage(request.getMessage());
         BdeoInspectionResponse response = new BdeoInspectionResponse();
         response.setMessage(responseMessage);
